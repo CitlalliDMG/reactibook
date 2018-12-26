8c3b6033738dfcce1194d2d8d4ca41faa02a88dc
Citlalli DMG <citlallidmg@gmail.com>
2018-12-26 15:56:29 -0600

    uploads sign up funcionality

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -1,5 +1,7 @@
   import configuration from "../../config";
   import app from "firebase/app";
+  import "firebase/auth";
+import { timingSafeEqual } from "crypto";
 
   // Initialize Firebase
   const config = {
@@ -14,7 +16,23 @@
   class Firebase {
     constructor(){
       app.initializeApp(config);
+
+      this.auth = app.auth();
     }
-  }
+
+    // AUTH FUNCTIONS
+
+    doCreateUserWithEmailAndPassword = (email, password) => 
+      this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+      this.auth.signInWithEmailAndPassword(email, password);
+    
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password => this.auth.currentUser.updatePassword(password);
+   }
 
 export default Firebase;
