b5a7d6455150aaa149b249248c2ec25d30490233
Citlalli DMG <citlallidmg@gmail.com>
2018-12-26 13:49:48 -0600

    uploads basic folder configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+// Adding bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+// eslint-disable-next-line
+import $ from 'jquery';
+// eslint-disable-next-line
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
